86341e908c3e693f0dfb5db58c24ac8607bb20a6
Bruno <brunodemedeiros14@gmail.com>
2023-07-29 01:15:16 -0300

    build: add spring dependencies

---
+++ juju_market/src/main/kotlin/com/brunomedeiros/juju_market/JujuMarketApplication.kt
@@ -0,0 +1,12 @@
+package com.brunomedeiros.juju_market
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JujuMarketApplication
+
+fun main(args: Array<String>) {
+
+	runApplication<JujuMarketApplication>(*args)
+}
---
+++ juju_market/src/test/kotlin/com/brunomedeiros/juju_market/JujuMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.brunomedeiros.juju_market
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JujuMarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
