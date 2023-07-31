506ecd24227341d1673f68559a3b3b01f5516354
Bruno <brunodemedeiros14@gmail.com>
2023-07-31 13:01:43 -0300

    feat: include seed migration

--- juju_market/src/main/resources/db/migration/V7__input-values.sql
+++ juju_market/src/main/resources/db/migration/V7__input-values.sql
@@ -4,9 +4,14 @@ INSERT INTO tb_category (category_name) VALUES
 ('Higiene');
 
 INSERT INTO tb_product (measurement_unit, product_name, unit_price, category_id) VALUES
-('un', 'Vassoura', 102.39, 1),
-('un', 'Toalha', 44.24, 3);
+('un', 'Vassoura', 12.39, 1),
+('un', 'Toalha', 22.24, 3);
 
 INSERT INTO tb_cart (payment_status) VALUES
 ('AWAITING'),
 ('AWAITING');
+
+INSERT INTO tb_product_cart (product_id, quantity, acquisition_price, cart_id) values
+(1, 3, 22.24, 1),
+(2, 4, 22.24, 1),
+(2, 5, 22.24, 2);
